aae0d19e5b875a765eafc0d407dc446552379661
Hugh Kennedy <hughskennedy@gmail.com>
2013-08-24 23:29:59 +1000

    Exclude `raf` for now.
    The module calls requestAnimationFrame *after* the event logic,
    which can lead to slight delays in the loop.

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
-var raf = require('raf')
-  , EventEmitter = require('events').EventEmitter
+var EventEmitter = require('events').EventEmitter
+
+var _raf =
+  global.requestAnimationFrame ||
+  global.webkitRequestAnimationFrame ||
+  global.mozRequestAnimationFrame ||
+  global.msRequestAnimationFrame ||
+  global.oRequestAnimationFrame
 
 module.exports = ticker
 
+var currtime =
+  global.performance &&
+  global.performance.now ? function() {
+    return performance.now()
+  } : Date.now || function () {
+    return +new Date
+  }
+
 function ticker(element, rate, limit) {
-  var millisecondsPerFrame = 1000 / (rate || 60)
+  var fps = 1000 / (rate || 60)
+    , emitter = new EventEmitter
+    , last = currtime()
     , time = 0
-    , emitter
+
+  var raf = _raf || function(fn, el) {
+    setTimeout(fn, fps)
+  }
 
   limit = arguments.length > 2 ? +limit + 1 : 2
-  emitter = raf(element || window).on('data', function(dt) {
+
+  function loop() {
+    raf(loop, element || null)
+
+    var now = currtime()
+    var dt = now - last
     var n = limit
 
+    emitter.emit('data', dt)
     time += dt
-    while (time > millisecondsPerFrame && n) {
-      time -= millisecondsPerFrame
+    while (time > fps && n) {
+      time -= fps
       n -= 1
-      emitter.emit('tick')
+      emitter.emit('tick', fps)
     }
-    time = (time + millisecondsPerFrame * 1000) % millisecondsPerFrame
 
-    if (n !== limit) emitter.emit('draw')
-  })
+    time = (time + fps * 1000) % fps
+    if (n !== limit) emitter.emit('draw', time / fps)
+    last = now
+  }
+
+  loop()
 
   return emitter
 }
